8bc9253e622a7d18633f9efdef494113baedcbaf
Lorri <zouyoo@outlook.com>
2026-03-17 00:35:41 +0000

    We should use the rocm version of llvm dev

--- init_build.sh
+++ init_build.sh
@@ -6,7 +6,8 @@ set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
-
+apt update && apt install rocm-llvm-dev
+export LLVM_DIR=/opt/rocm/llvm
 echo "=== Building TensileLite client ==="
 cd hipblaslt/tensilelite/
 pip3 install invoke
